cb5cfbe46ab7339e897870e8f41a5144049c26de
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-12-27 00:51:35 -0800

    Change NextRenderer to use an experimemtal double-res mode
    I will probably revert this commit, but copy these changes to a new file. There's some useful things this does, but it's much slower and small details can get lost.

--- src/main/java/voxswirl/visual/NextRenderer.java
+++ src/main/java/voxswirl/visual/NextRenderer.java
@@ -52,8 +52,8 @@ public class NextRenderer {
         colorI = fill(-1f, w, h);
         colorP = fill(-1f, w, h);
         colorT = fill(-1f, w, h);
-        remade = new byte[size << 1][size << 1][size << 1];
-        lights = new float[size << 1][size << 1][size << 1];
+        remade = new byte[size << 2][size << 2][size << 2];
+        lights = new float[size << 2][size << 2][size << 2];
         Tools3D.fill(lights, 0.75f);
     }
     
@@ -149,13 +149,14 @@ public class NextRenderer {
     
     public void splat(float xPos, float yPos, float zPos, byte voxel) {
 //        remade[(int) ((xPos + 0x1p23f) - 0x1p23f)][(int) ((yPos + 0x1p23f) - 0x1p23f)][(int) ((zPos + 0x1p23f) - 0x1p23f)] = voxel;
-        for (int xp = (int) xPos; xp < xPos + 0.5f; xp++) {
-            for (int yp = (int) yPos; yp < yPos + 0.5f; yp++) {
-                for (int zp = (int) zPos; zp < zPos + 0.5f; zp++) {
-                    remade[xp][yp][zp] = voxel;
-                }
-            }
-        }
+        remade[(int) xPos + size][(int) yPos + size][(int) zPos] = voxel;
+//        for (int xp = (int) xPos; xp < xPos + 1f; xp++) {
+//            for (int yp = (int) yPos; yp < yPos + 1f; yp++) {
+//                for (int zp = (int) zPos; zp < zPos + 1f; zp++) {
+//                    remade[xp][yp][zp] = voxel;
+//                }
+//            }
+//        }
     }
     
     public NextRenderer clear() {
@@ -186,14 +187,14 @@ public class NextRenderer {
                 weakMinor = 0.05f * 12f / lightPasses;
         pixmap.setColor(0);
         pixmap.fill();
-        final int xSize = render.length - 1, ySize = render[0].length - 1,
+        final int xSize = render.length - 1, ySize = render[0].length - 1, size2 = size << 1,
                 rmxLength = remade.length, rmyLength = remade[0].length, rmzLength = remade[0][0].length,
-                startRegion = size >> 1, endRegion = rmxLength - (size >> 1);
+                startRegion = size, endRegion = rmxLength - (size);
         int xx, yy, depth, voxel, vx, vy, vz;
         VoxMaterial m;
 
         // top lighting
-        int starting = size * 3 >> 1;
+        int starting = size * 3;
         for (int y = startRegion; y < endRegion; y++) {
             for (int x = startRegion; x < endRegion; x++) {
                 for (int p = 0; p < lightPasses; p++) {
@@ -201,7 +202,7 @@ public class NextRenderer {
                     for (int z = starting;
                          z >= 0 && ox >= startRegion && oy >= startRegion && ox < endRegion && oy < endRegion;
                          z--, ox += xAngle, oy += yAngle) {
-                        if(z >= size) continue;
+                        if(z >= size2) continue;
                         vx = (int)(ox + 0.5f);
                         vy = (int)(oy + 0.5f);
                         if((voxel = remade[vx][vy][z] & 255) != 0){
@@ -229,12 +230,12 @@ public class NextRenderer {
         }
         // side lighting
         starting = endRegion;
-        for (int z = 0; z < size; z++) {
+        for (int z = 0; z < size2; z++) {
             for (int y = startRegion; y < endRegion; y++) {
                 for (int p = 0; p < lightPasses; p++) {
                     float oz = z, oy = y, zAngle= biasedAngle(y + 11, z + 47, -p), yAngle = biasedAngle(y + 19, z + 13, p);
                     for (int x = starting;
-                         x >= 0 && oz >= 0 && oy >= startRegion && oz < size && oy < endRegion;
+                         x >= 0 && oz >= 0 && oy >= startRegion && oz < size2 && oy < endRegion;
                          x--, oz += zAngle, oy += yAngle) {
                         vz = (int)(oz + 0.5f);
                         vy = (int)(oy + 0.5f);
@@ -244,7 +245,7 @@ public class NextRenderer {
                             lights[x][vy][vz] += weakMain;
                             for (int vxx = x - 1; vxx <= x + 1; vxx++) {
                                 for (int vyy = vy - 1; vyy <= vy + 1; vyy++) {
-                                    for (int vzz = Math.max(0, vz - 1); vzz <= Math.min(size, vz + 1); vzz++) {
+                                    for (int vzz = Math.max(0, vz - 1); vzz <= Math.min(size2, vz + 1); vzz++) {
                                         lights[vxx][vyy][vzz] += carry;
                                     }
                                 }
@@ -263,9 +264,11 @@ public class NextRenderer {
         for (int z = 0; z < remade[0][0].length; z++) {
             for (int y = 0; y < remade[0].length; y++) {
                 for (int x = 0; x < remade.length; x++) {
-                    xx = (size + y - x) * 2 + 1;
+//                    xx = (size + y - x) * 2 + 1;
+                    xx = (size + y - x) + 1;
                     if(xx < 0 || xx > xSize) continue;
-                    yy = (z * 3 + size * 3 - x - y) + 1;
+//                    yy = (z * 3 + size * 3 - x - y) + 1;
+                    yy = (z * 3 + size * 5 - x - y) + 2 >> 1;
                     if(yy < 0 || yy > ySize) continue;
                     voxel = remade[x][y][z] & 255;
                     if(voxel == 0) continue;
@@ -274,9 +277,9 @@ public class NextRenderer {
                     final float emit = m.getTrait(VoxMaterial.MaterialTrait._emit) * 1.25f;
                     final float alpha = m.getTrait(VoxMaterial.MaterialTrait._alpha);
                     final float reflect = m.getTrait(VoxMaterial.MaterialTrait._ior) + 0.9375f;
-
-                    for (int lx = 0, ax = xx; lx < 4 && ax <= xSize; lx++, ax++) {
-                        for (int ly = 0, ay = yy; ly < 4 && ay <= ySize; ly++, ay++) {
+//                    final int ax = xx, ay = yy;
+                    for (int lx = 0, ax = xx; lx < 2 && ax <= xSize; lx++, ax++) {
+                        for (int ly = 0, ay = yy; ly < 2 && ay <= ySize; ly++, ay++) {
                             if (depth >= depths[ax][ay] && (alpha == 0f || bn(ax >>> 1, ay >>> 1) >= alpha)) {
                                 colorI[ax][ay] = (float)Math.pow(paletteI[voxel] * (float) Math.sqrt(lights[x][y][z]), reflect);
                                 colorP[ax][ay] = paletteP[voxel];
@@ -301,7 +304,8 @@ public class NextRenderer {
         for (int x = 0; x <= xSize; x++) {
             for (int y = 0; y <= ySize; y++) {
                 if (colorP[x][y] >= 0f) {
-                    pixmap.drawPixel(x >>> 1, y >>> 1, render[x][y] = ColorTools.toRGBA8888(ColorTools.ipt(
+//                    pixmap.drawPixel(x >>> 1, y >>> 1, render[x][y] = ColorTools.toRGBA8888(ColorTools.ipt(
+                    pixmap.drawPixel(x, y, render[x][y] = ColorTools.toRGBA8888(ColorTools.ipt(
                             Math.min(Math.max(colorI[x][y], 0f), 1f),
                             (colorP[x][y] - 0.5f) * neutral + 0.5f,
                             (colorT[x][y] - 0.5f) * neutral + 0.5f, 1f)));
@@ -311,11 +315,13 @@ public class NextRenderer {
         if (outline) {
             int o;
             for (int x = 2; x < xSize - 1; x++) {
-                final int hx = x >>> 1;
+                final int hx = x;
+//                final int hx = x >>> 1;
                 for (int y = 2; y < ySize - 1; y++) {
-                    int hy = y >>> 1;
+                    final int hy = y;
+//                    int hy = y >>> 1;
                     if ((o = outlines[x][y]) != 0) {
-                        depth = depths[x][y] - threshold;
+                        depth = depths[x][y] - (threshold << 1);
                         if (outlines[x - 1][y] == 0 || depths[x - 1][y] < depth) {
                             pixmap.drawPixel(hx, hy    , o);
                         }
@@ -361,7 +367,7 @@ public class NextRenderer {
         seed += TimeUtils.millis() * 0x632BE59BD9B4E019L;
 //        seed = Tools3D.hash64(colors);
         final int size = colors.length;
-        final float hs = (size) * 0.5f;
+        final float hs = size * 0.5f;
         final float c = cos_(angleTurns), s = sin_(angleTurns);
 //        float minX = Float.POSITIVE_INFINITY, maxX = Float.NEGATIVE_INFINITY;
 //        float minY = Float.POSITIVE_INFINITY, maxY = Float.NEGATIVE_INFINITY;
@@ -371,15 +377,15 @@ public class NextRenderer {
                     final byte v = colors[x][y][z];
                     if(v != 0)
                     {
-//                        for (int m = 0; m < 2; m++) {
-//                            for (int n = 0; n < 2; n++) {
-//                                for (int o = 0; o < 2; o++) {
-                                    final float xPos = (x-hs) * c - (y-hs) * s + size;
-                                    final float yPos = (x-hs) * s + (y-hs) * c + size;
-                                    splat(xPos, yPos, z, v);
-//                                }
-//                            }
-//                        }
+                        for (int m = 0; m < 2; m++) {
+                            for (int n = 0; n < 2; n++) {
+                                for (int o = 0; o < 2; o++) {
+                                    final float xPos = (x+m-hs) * c - (y+n-hs) * s + size;
+                                    final float yPos = (x+m-hs) * s + (y+n-hs) * c + size;
+                                    splat(xPos, yPos, z + o, v);
+                                }
+                            }
+                        }
 //                        minX = Math.min(minX, xPos);
 //                        maxX = Math.max(maxX, xPos);
 //                        minY = Math.min(minY, yPos);
